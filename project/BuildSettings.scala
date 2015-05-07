62cd143d1c3633a7e2aa1d4272ec85f91ff5f604
ipostanogov <ipostanogov@outlook.com>
2015-05-07 14:24:22 +0500

    Fix deps

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -1,9 +1,14 @@
 import sbt.Keys._
+import sbt.Resolver
 
 object BuildSettings {
 
   lazy val basicSettings = Seq(
     version := "0.1-SNAPSHOT",
+    resolvers ++= Seq(
+      Resolver.sonatypeRepo("releases"),
+      Resolver.typesafeRepo("releases")
+    ),
     scalaVersion := "2.11.6"
   )
 }
